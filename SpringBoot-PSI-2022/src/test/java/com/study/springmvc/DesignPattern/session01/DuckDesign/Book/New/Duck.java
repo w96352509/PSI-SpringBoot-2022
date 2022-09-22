71e33095df61749f2e17dc70eaef5f60b66263a3
vic <vichsu38@gmail.com>
2022-09-22 17:49:58 +0800

    0922

--- SpringBoot-PSI-2022/src/test/java/com/study/springmvc/DesignPattern/session01/DuckDesign/Book/New/Duck.java
+++ SpringBoot-PSI-2022/src/test/java/com/study/springmvc/DesignPattern/session01/DuckDesign/Book/New/Duck.java
@@ -5,13 +5,47 @@ import com.study.springmvc.DesignPattern.session01.DuckDesign.Book.New.Interface
 
 public abstract class Duck {
 
+	private Fly fly;
+	
+	private Quack quack;
+	
+	public void setFunction(FType ftype , QType qType) {
+		switch (qType) {
+		case YQ:
+			this.quack = new YQuack();
+			break;
+
+		case NQ:
+			this.quack = new NQuack();
+			break;
+		}
+		
+		switch (ftype) {
+		case YF:
+			this.fly = new YFly();
+			break;
+
+		case NF:
+			this.fly = new NFly();
+			break;
+		}
+	}
+	
+	enum FType{
+		YF , NF
+	}
+	
+	enum QType{
+		YQ , NQ 
+	}
+	
 	private void swim() {
 		System.out.println("游");
 	}
 	
 	public abstract void display();
 	
-	public void total(Fly fly , Quack quack) {
+	public void total() {
 		swim();
 		display();
 		fly.fly();
--- SpringBoot-PSI-2022/src/test/java/com/study/springmvc/DesignPattern/session01/DuckDesign/Book/New/Main.java
+++ SpringBoot-PSI-2022/src/test/java/com/study/springmvc/DesignPattern/session01/DuckDesign/Book/New/Main.java
@@ -1,10 +1,15 @@
 package com.study.springmvc.DesignPattern.session01.DuckDesign.Book.New;
 
+import com.study.springmvc.DesignPattern.session01.DuckDesign.Book.New.Duck.FType;
+import com.study.springmvc.DesignPattern.session01.DuckDesign.Book.New.Duck.QType;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Duck RubberDuck = new RubberDuck();
-		RubberDuck.total(new NFly(), new YQuack());
+		Duck rubberDuck = new RubberDuck();
+		rubberDuck.setFunction(FType.NF , QType.NQ );
+		rubberDuck.total();
+		
 	}
 	
 }
